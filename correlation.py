39961f2123a6eb13c868a9e4b1bdf37846c380aa
Lodovico Caruso <lodovico.caruso@gmail.com>
2025-08-15 01:28:56 +0200

    summary upgrade with correlation matrix

--- correlation.py
+++ correlation.py
@@ -6,8 +6,14 @@ summary = pd.read_csv('summary.csv', index_col=0)
 cumulative_returns_outer = pd.read_csv('cumulative_returns_outer.txt', index_col=0)
 
 
-correlation_matrix = cumulative_returns_outer.corr()
+correlation_matrix = cumulative_returns_outer.corr().round(2)
 
 # Stampo la matrice di correlazione
 print("Matrice di correlazione tra i rendimenti:")
 print(correlation_matrix)
+correlation_matrix.index.name = "Name"  # Nome per la "prima colonna"
+
+summary = pd.merge(summary, correlation_matrix, on = 'Name')
+summary.to_csv("summary.csv")
+print("\nFile aggiornato, ti mostro summary.csv, ho aggiunto un po' di informazioni utili:\n")
+print(summary)

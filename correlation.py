264620c26146ae62bb60295d264588946cd04be1
Lodovico Caruso <lodovico.caruso@gmail.com>
2025-08-15 02:10:35 +0200

    correlation lil correction

--- correlation.py
+++ correlation.py
@@ -2,15 +2,18 @@ import pandas as pd
 import numpy as np
 
 #input
-summary = pd.read_csv('summary.csv', index_col=0) 
-cumulative_returns_outer = pd.read_csv('cumulative_returns_outer.txt', index_col=0)
+summary = pd.read_csv(
+    "summary.csv",
+    index_col=0,
+    usecols=range(7)  # legge solo le prime 4 colonne (indice 0,1,2,3)
+) #questo è importante se hai già calcolato la correlazione, dato che alla fine il codice fa un merge
 
+cumulative_returns_outer = pd.read_csv('cumulative_returns_outer.txt', index_col=0)
 
 correlation_matrix = cumulative_returns_outer.corr().round(2)
 
 # Stampo la matrice di correlazione
 print("Matrice di correlazione tra i rendimenti:")
-print(correlation_matrix)
 correlation_matrix.index.name = "Name"  # Nome per la "prima colonna"
 
 summary = pd.merge(summary, correlation_matrix, on = 'Name')
